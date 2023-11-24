d32cd1d50bb6c7889fbdfbd8639074c299960821
Berke Akçen <berkeakcen@gmail.com>
2023-11-24 15:37:24 +0300

    Added text centering

--- src/main/java/me/despical/battleacademy/menus/elements/SelectComponents.java
+++ src/main/java/me/despical/battleacademy/menus/elements/SelectComponents.java
@@ -1,5 +1,6 @@
 package me.despical.battleacademy.menus.elements;
 
+import me.despical.battleacademy.util.Utils;
 import me.despical.commons.compat.XMaterial;
 import me.despical.commons.configuration.ConfigUtils;
 import me.despical.commons.item.ItemBuilder;
@@ -10,9 +11,10 @@ public class SelectComponents {
 	public void injectComponents(SelectMenu menu) {
 		final var config = ConfigUtils.getConfig(menu.plugin, "menu");
 
+		var fireItemLore = config.getStringList("element-menu.fire-element.lore");
 		var fireItem = new ItemBuilder(XMaterial.matchXMaterial(config.getString("element-menu.fire-element.material")).orElse(XMaterial.CAMPFIRE))
-			.name(config.getString("element-menu.fire-element.name"))
-			.lore(config.getStringList("element-menu.fire-element.lore"))
+			.name(Utils.center(config.getString("element-menu.fire-element.name"), fireItemLore))
+			.lore(fireItemLore)
 			.build();
 
 		menu.pane.addItem(GuiItem.of(fireItem), 1, 1);
--- src/main/java/me/despical/battleacademy/util/Utils.java
+++ src/main/java/me/despical/battleacademy/util/Utils.java
@@ -4,7 +4,32 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class Utils {
+
+	public static String center(String title, List<String> lore) {
+		var j = lore.stream().map(Utils::lengthWithoutColors).max(Comparator.naturalOrder()).get();
+		var spaces = j / 2 + lengthWithoutColors(title);
+
+		return " ".repeat(Math.max(0, spaces)) + title;
+	}
+
+	public static int lengthWithoutColors(String message) {
+		var pattern = Pattern.compile("#[a-fA-F0-9]{6}");
+		var matcher = pattern.matcher(message);
+
+		while (matcher.find()) {
+			String hexCode = message.substring(matcher.start(), matcher.end());
+			message = message.replace(hexCode,  "");
+			matcher = pattern.matcher(message);
+			message = message.replaceAll("(?i)" + '&' + "[0-9A-FK-ORX]", "");
+		}
+
+		return message.length();
+	}
 
 	public static boolean isCriticalHit(Player player) {
 		return
