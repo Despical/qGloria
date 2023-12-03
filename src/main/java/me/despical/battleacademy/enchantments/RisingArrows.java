9551f872f13a463b0ab6629bc87a235930d50686
Berke Akçen <berkeakcen@gmail.com>
2023-12-03 20:43:26 +0300

    Fixed rising arrows not working

--- src/main/java/me/despical/battleacademy/enchantments/RisingArrows.java
+++ src/main/java/me/despical/battleacademy/enchantments/RisingArrows.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -26,13 +27,22 @@ public class RisingArrows extends CustomEnchantment {
 		return new Listener() {
 
 			@EventHandler
-			public void onDamage(EntityDamageByEntityEvent event) {
-				if (!(event.getDamager() instanceof Arrow arrow && arrow.getShooter() instanceof Player shooter && event.getEntity() instanceof Player player)) return;
-				if (arrow.getItemStack().getItemMeta() == null) return;
-				if (!arrow.getItemStack().getItemMeta().hasEnchant(RisingArrows.this)) return;
+			public void onShootBow(EntityShootBowEvent event) {
+				if (!(event.getEntity() instanceof Player shooter)) return;
+				if (event.getBow().getItemMeta() == null) return;
+				if (!event.getBow().getItemMeta().hasEnchant(RisingArrows.this)) return;
 				if (!plugin.getUserManager().getUser(shooter).isEnchantmentCompatible(RisingArrows.this)) return;
 
-				player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 100, 0, false, false, false));
+				event.getProjectile().setCustomName("rising_arrow");
+			}
+
+			@EventHandler
+			public void onDamage(EntityDamageByEntityEvent event) {
+				if (!(event.getDamager() instanceof Arrow arrow && arrow.getShooter() instanceof Player shooter && event.getEntity() instanceof Player player)) return;
+				if (!plugin.getUserManager().getUser(shooter).isEnchantmentCompatible(RisingArrows.this)) return;
+				if (!arrow.getCustomName().equals("rising_arrow")) return;
+
+				player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 100, 1, false, false, false));
 			}
 		};
 	}
@@ -57,4 +67,3 @@ public class RisingArrows extends CustomEnchantment {
 		return Set.of(EquipmentSlot.HAND);
 	}
 }
-
